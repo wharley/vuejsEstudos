a82df097c110575897a9eb731ec27b42d7651081
wharley <wharleyangelica@gmail.com>
2016-11-25 16:51:33 -0200

    Estudo Vuejs 2.0

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+      <RootHeader />
+      <div class="container">
+        <router-view></router-view>
+      </div>
+
+  </div>
+</template>
+
+<script>
+import RootHeader from './components/root/header.vue'
+
+export default {
+  components: { RootHeader }
+}
+</script>
+
+<style src="assets/sass/app.scss" lang="sass"></style>
