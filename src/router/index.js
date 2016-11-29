d04474c322a1115d8ac0a08fda12a11833018570
wharley <wharleyangelica@gmail.com>
2016-11-29 13:24:06 -0200

    Estudo Vuejs 2.0 - Protegendo router

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import routes from './routes'
+import beforeEach from './beforeEach'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({ routes, linkActiveClass: 'active' })
+
+router.beforeEach(beforeEach)
 
 export default router
