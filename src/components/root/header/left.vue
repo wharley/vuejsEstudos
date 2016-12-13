e838d428da3080aa8057b72f9435de65355946a8
wharley <wharleyangelica@gmail.com>
2016-12-13 00:08:49 -0200

    Estudo Vuejs 2.0 - Menu Cadastro

--- src/components/root/header/left.vue
+++ src/components/root/header/left.vue
@@ -3,6 +3,9 @@
       <router-link :to="{ name: 'dashboard.main' }" tag="li" exact>
         <a>Home</a>
       </router-link>
+      <router-link :to="{ name: 'dashboard.main' }" tag="li" exact>
+        <a>Cadastros</a>
+      </router-link>
   </ul>
 </template>
 
